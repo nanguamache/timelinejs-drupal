c13169cc8871f4c3e4628863afcd87b367f2551d
Steffen Rühlmann <s.ruehlmann@hks-agentur.de>
2012-09-13 17:06:18 +0200

    first commit

---
+++ js/timelinejs.js
@@ -0,0 +1,27 @@
+Drupal.timelinejs = {
+  timerID: null,
+  getTheme: function(theme, timeline_start, timeline_end) {
+    var theme = TimelineJs[theme].create();
+    return theme;
+  },
+  createWidget: function(id,args) {
+   args = eval(args);
+   console.log(args);
+   var timeline = new VMM.Timeline(id);
+   timeline.init({source:args} );
+  }
+};
+
+Drupal.behaviors.timelinejs = {
+  attach:  function(context) {
+    if(VMM.Timeline && Drupal.settings.timelinejs) {
+      jQuery.each(Drupal.settings.timelinejs, function(i, v) {
+        jQuery.each(v, function(id, data) {
+           Drupal.timelinejs.createWidget(id,data);
+        });
+
+      });
+    }
+  }
+};
+
---
+++ views-view-timelinejs.tpl.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * @file
+ * This template includes the timeline view.
+ */
+?>
+<div class="timelinejs-wrapper">
+  <noscript><p><?php print t('This page uses Javascript to show you a Timeline. Please enable Javascript in your browser to see the full page. Thank you.'); ?></p></noscript>
+  <div id="<?php print $id; ?>" class="<?php print $timelinejs_theme ?>"></div>
+</div>
